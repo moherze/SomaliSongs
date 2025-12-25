13d87ac077a75491fc44a1398887df6dfe19ce54
moherze <moha.cqaadir@gmail.com>
2025-12-25 19:45:20 +0300

    added a header

---
+++ main.js
@@ -0,0 +1,61 @@
+
+
+const navBar = document.querySelector('.nav-bar')
+const Button = document.querySelector('.toggle-button');
+// const navLinks = document.querySelector('.navbar-links')
+const aLinks = document.querySelectorAll('.navbar-links a')
+
+
+
+Button.addEventListener('click', ()=>{
+    navBar.classList.toggle('active')
+  
+})
+
+aLinks.forEach(link => {
+    link.addEventListener('click', function(){
+         navBar.classList.remove('active')
+    })
+});
+
+const toggleButton = document.querySelector("#mode-toggle")
+
+toggleButton.addEventListener('click', ()=>{
+  
+    document.body.classList.toggle("dark-mode")
+    toggleButton.classList.toggle("dark-mode")
+
+   
+ if(toggleButton.classList.contains('dark-mode')){
+    toggleButton.textContent= "⏾"
+    toggleButton.style.color= "black"
+    localStorage.setItem('mode', 'dark')
+
+ }else{
+    toggleButton.textContent= "🔆"
+    
+    localStorage.setItem("mode", "light")
+ }
+})
+
+window.addEventListener("DOMContentLoaded", ()=>{
+ 
+    const SaveMode = localStorage.getItem('mode')
+
+    if(SaveMode === "dark"){
+        document.body.classList.add('dark-mode')
+        toggleButton.classList.add("dark-mode")
+        toggleButton.textContent= "⏾"
+         toggleButton.style.color= "black"
+    
+
+
+        
+    }else{
+         toggleButton.textContent= "🔆"
+    }
+})
+
+
+
+
