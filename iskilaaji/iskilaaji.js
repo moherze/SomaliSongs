9785ff7043bbe66859de4d6c434bef8368fc5d22
moherze <moha.cqaadir@gmail.com>
2025-12-28 20:06:39 +0300

    added iskilaaaji js videos

--- iskilaaji/iskilaaji.js
+++ iskilaaji/iskilaaji.js
@@ -57,5 +57,196 @@ window.addEventListener("DOMContentLoaded", ()=>{
 })
 
 
+//  video player 
 
+
+ const videoElement = document.getElementById('video')
+
+
+
+
+// Select DOM elements
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const title = document.getElementById('title');
+const year = document.getElementById('year');
+const cover = document.getElementById('cover');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-bar');
+const volumeSlider = document.getElementById('volume');
+const speedSelect = document.getElementById('speed');
+
+
+
+// Songs Array
+const videos = [
+    {
+        title: "Ha ii jabin ruux i neceb", year: "2020", src: "videoes/Ha ii Jabin Roox i Neceb.mp4"
+    },
+    {
+        title: "Fariin",  year: "2019", src: "videoes/FARRIIN 2020.mp4"
+    },
+    {
+        title: "Ha Idilin rabow Ha i Doorin",  year: "2024", src: "videoes/HA IDILIN RABOOW HA I DOORIN .mp4"
+    },
+
+     {   title: "video jaamac",  year: "jaamac", src: "videoes/SOOHDIN KAMA XIRAN WALI.mp4"
+    },
+]
+
+
+
+let videosIndex = 0;
+let isPlaying = false;
+let speed = 1;
+
+
+// All Functions
+
+function loadVideo(video){
+
+    title.textContent = video.title;
+    // year.textContent = video.year;
+    // videoElement.poster = video.cover;
+    videoElement.src = video.src;       
+}
+
+loadVideo(videos[videosIndex])
+
+
+
+function playVideo(){
+    playBtn.querySelector('i').classList.remove('fa-play')
+    playBtn.querySelector('i').classList.add('fa-pause')
+    videoElement.play();
+    isPlaying = true;
+}
+
+function pauseVideo(){
+    playBtn.querySelector('i').classList.remove('fa-pause')
+    playBtn.querySelector('i').classList.add('fa-play')
+    videoElement.pause();
+    isPlaying = false;
+}
+
+function nextVideo(){
+    pauseVideo();
+    setTimeout( ()=> {
+
+        videosIndex++   
+        if(videosIndex > videos.length - 1){
+            videosIndex = 0;
+        }
+
+        loadVideo(videos[videosIndex])
+        playVideo();
+
+    }, 300)
+    
+
+}
+
+function prevVideo(){
+    pauseVideo();
+    setTimeout( ()=> {
+
+        videosIndex--  
+        if(videosIndex < 0){
+            videosIndex = videos.length - 1;
+        }
+
+        loadVideo(videos[videosIndex])
+        playVideo();
+
+    }, 300)
+    
+
+}
+
+
+// Progress function
+
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement;    
+    if(isNaN(duration)) return;
+    const progressPercent = (currentTime / duration)*100;
+    progress.style.width = `${progressPercent}%`;
+
+    // Calculating display for duration
+    const durationMinuts = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if(durationSeconds < 10){
+        durationSeconds = `0${durationSeconds}`;
+    }
+    currentTimeEl.textContent = `${durationMinuts}: ${durationSeconds}`;
+
+    // Calculating display for duration
+    const currentMinuts = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if(currentSeconds < 10){
+        currentSeconds = `0${currentSeconds}`;
+    }
+
+    durationEl.textContent = `${currentMinuts}:${currentSeconds}`
+
+    videoElement.playbackRate = speed;
+
+}
+
+function setProgress(e){
+
+    const width = this.clientWidth;   
+    const clickX = e.offsetX;
+    const duration = videoElement.duration;
+    const newTime = (clickX / width) * duration;
+    videoElement.currentTime = newTime;     
+
+}
+
+
+
+// All Event Listners
+
+playBtn.addEventListener('click', ()=> {
+
+    if(isPlaying){
+        pauseVideo();
+    }else{        
+        playVideo();
+    }   
+});
+
+// NextSong Button
+nextBtn.addEventListener('click', ()=> {
+    nextVideo();
+})
+
+prevBtn.addEventListener('click', ()=> {
+    prevVideo();
+})
+
+videoElement.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+volumeSlider.addEventListener('input', (e)=>{
+    videoElement.volume = e.target.value;
+})
+
+speedSelect.addEventListener('change', (e)=>{
+    speed = parseFloat(e.target.value)
+    videoElement.playbackRate = speed;
+})
+
+videoElement.addEventListener('ended', ()=>{
+    playBtn.querySelector('i').classList.remove('fa-pause')
+    playBtn.querySelector('i').classList.add('fa-play')    
+    isPlaying = false;
+   
+})
+
+videoElement.addEventListener('loadedmetadata', updateProgress);
 
